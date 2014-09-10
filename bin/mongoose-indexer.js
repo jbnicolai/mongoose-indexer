ac52f73e35c178cd4539527c92d33b5da9ca2abb
Henry Liu <thenrikie@hotmail.com>
2014-09-11 04:35:22 +0800

    fix absolute path detection

--- bin/mongoose-indexer.js
+++ bin/mongoose-indexer.js
@@ -5,9 +5,11 @@ var argv = require('yargs')
             .usage('$0 -username [username] --password [password] --host [host] --port [port] [database] [files]')
             .example('$0 db1 models/*.js', 'Create indexes for all models in models folder in database db1')
             .demand(2)
+            .default('host', 'localhost')
             .argv;
 
 argv.database = argv._[0];
 argv.files = argv._.slice(1);
+argv.passMongooseObject = true;
 
 mongooseIndexer(argv);
