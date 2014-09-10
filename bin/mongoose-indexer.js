1bfed804919fec550777f0059d62f05fbf2b32ac
Henry Liu <thenrikie@hotmail.com>
2014-09-10 17:49:11 +0800

    cli

--- bin/mongoose-indexer.js
+++ bin/mongoose-indexer.js
@@ -1,6 +1,13 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+#!/usr/bin/env node
 
+var mongooseIndexer = require("../lib/");
+var argv = require('yargs')
+            .usage('$0 -username [username] --password [password] --host [host] --port [port] [database] [files]')
+            .example('$0 db1 models/*.js', 'Create indexes for all models in models folder in database db1')
+            .demand(2)
+            .argv;
 
+argv.database = argv._[0];
+argv.files = argv._.slice(1);
+
+mongooseIndexer(argv);
